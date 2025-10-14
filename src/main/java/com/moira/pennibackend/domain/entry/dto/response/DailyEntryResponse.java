142e598bbd1d8b55e69c9e64e1116f8d2d54a50c
hunzzzzz <hun819.st@gmail.com>
2025-10-14 21:08:18 +0900

    누락된 필드 추가

--- src/main/java/com/moira/pennibackend/domain/entry/dto/response/DailyEntryResponse.java
+++ src/main/java/com/moira/pennibackend/domain/entry/dto/response/DailyEntryResponse.java
@@ -1,15 +1,19 @@
 package com.moira.pennibackend.domain.entry.dto.response;
 
 public record DailyEntryResponse(
+        // 가계부 항목 관련
         String id,
         String type,
         String content,
         String description,
         Long price,
+        String method,
 
+        // 카테고리 관련
         Long categoryId,
         String categoryName,
 
+        // 유저 관련
         String userId,
         String userNickname
 ) {
